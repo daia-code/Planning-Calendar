9da3c960e5eb865116f8979a4e410e1a09b42075
Lucaciu Daiana-Larisa <78080241+daia-code@users.noreply.github.com>
2022-10-11 22:07:11 +0300

    Update accountcreate.view.js

--- WebContent/timebee/accountcreate.view.js
+++ WebContent/timebee/accountcreate.view.js
@@ -1,162 +1,70 @@
-sap.ui.controller("timebee.accountcreate", {
+sap.ui.jsview("timebee.accountcreate", {
 
 	/**
-	 * Called when a controller is instantiated and its View controls (if
-	 * available) are already created. Can be used to modify the View before it
-	 * is displayed, to bind event handlers and do other one-time
-	 * initialization.
+	 * Specifies the Controller belonging to this View. In the case that it is
+	 * not implemented, or that "null" is returned, this View does not have a
+	 * Controller.
 	 * 
 	 * @memberOf timebee.accountcreate
 	 */
-	onInit : function() {
-
+	getControllerName : function() {
+		return "timebee.accountcreate";
 	},
 
 	/**
-	 * Similar to onAfterRendering, but this hook is invoked before the
-	 * controller's View is re-rendered (NOT before the first rendering!
-	 * onInit() is used for that one!).
+	 * Is initially called once after the Controller has been instantiated. It
+	 * is the place where the UI is constructed. Since the Controller is given
+	 * to this method, its event handlers can be attached right away.
 	 * 
 	 * @memberOf timebee.accountcreate
 	 */
-	// onBeforeRendering: function() {
-	//
-	// },
-	/**
-	 * Called when the View has been rendered (so its HTML is part of the
-	 * document). Post-rendering manipulations of the HTML could be done here.
-	 * This hook is the same one that SAPUI5 controls get after being rendered.
-	 * 
-	 * @memberOf timebee.accountcreate
-	 */
-	// onAfterRendering: function() {
-	//
-	// },
-	onPressCreateDataAccount : function() {
-		var oEvents = [];
-		var sUrl = "http://localhost:5000/users";
-		var oDataToCreate = {
-			email : sap.ui.getCore().byId("idEmail").getValue(),
-			events : JSON.stringify(oEvents),
+	createContent : function(oController) {
+		var form = new sap.ui.layout.form.SimpleForm("form", {
+			content : [ new sap.m.Label({
+				text : "Name",
+				required : true,
 
-			name : sap.ui.getCore().byId("idName1").getValue(),
-			password : sap.ui.getCore().byId("idPass1").getValue()
-		};
+			}).addStyleClass("elementC1"),
+					new sap.m.Input("idName1").addStyleClass("elementC2"),
+					new sap.m.Label({
+						text : "Email",
+						required : true
+					}).addStyleClass("elementC1"),
+					new sap.m.Input("idEmail").addStyleClass("elementC2"),
+					new sap.m.Label({
+						text : "Password",
+						required : true
+					}).addStyleClass("elementC1"),
+					new sap.m.Input("idPass11", {
+						type : "Password"
 
-		jQuery.ajax({
-			type : "POST",
-			url : sUrl,
-			data : $.param(oDataToCreate),
-			contentType : 'application/x-www-form-urlencoded',
-			success : function() {
-				console.log("POST SUCCESS");
-			}.bind(this),
-			error : function(oError) {
-				console.log("ERROR POST");
-			}
+					}).addStyleClass("elementC2"), new sap.m.Label({
+						text : "Password",
+						required : true
+					}).addStyleClass("elementC1"), new sap.m.Input("idPass1", {
+						type : "Password"
+
+					}).addStyleClass("elementC2"),
+
+			]
+
+		})
+		var oBtn = new sap.m.Button({
+			text : "Create",
+			press : [ oController.createCont, oController ]
+		}).addStyleClass("styleBtnCreate");
+
+		var oPage = new sap.m.Page({
+			title : "Create account",
+			showNavButton : true,
+			navButtonPress : function() {
+				app.back();
+			},
+			content : [ form, oBtn
+
+			]
 		});
-	},
-	createCont : function(oEvt) {
+		return oPage;
 
-		var oModel = sap.ui.getCore().getModel("model");
-		var oData = oModel.getData();
-		var oInput = sap.ui.getCore().byId("idName1").getValue();
-		var oEmail = sap.ui.getCore().byId("idEmail").getValue();
-		var oPass = sap.ui.getCore().byId("idPass1").getValue();
-		var oPass2 = sap.ui.getCore().byId("idPass11").getValue();
-		var oLabel = sap.ui.getCore().byId("idLabel");
-		oLabel.setText(oInput);
-
-		var ok = 1;
-		console.log(oInput);
-		for (var i = 0; i < oData.users['length']; i++) {
-			if (oData.users[i].name == oInput) {
-				var dialog = new sap.m.Dialog({
-
-					title : "Error",
-					state : sap.ui.core.ValueState.Error,
-					content : [ new sap.m.Label({
-						text : "You have a account yet."
-					}),
-
-					new sap.m.Button({
-						type : sap.m.ButtonType.Emphasized,
-						text : "OK",
-						press : function() {
-							dialog.close();
-						}
-					}).addStyleClass("btnError2") ]
-				});
-				dialog.open();
-				ok--;
-			}
-
-		}
-		if (ok == 1) {
-			if (oInput != "" && oPass != "" && oEmail != "") {
-				if (oPass == oPass2) {
-					var id = oData.users['length'] + 1;
-					this.onPressCreateDataAccount();
-					var modelUser = sap.ui.getCore().getModel("user");
-
-					var userData = {
-						name : oInput,
-						email : oEmail,
-						password : oPass,
-					};
-
-					modelUser.setData(userData);
-
-					app.to("idtimebee3");
-				} else {
-					var dialog = new sap.m.Dialog({
-
-						title : "Warning",
-						state : sap.ui.core.ValueState.Warning,
-						content : [ new sap.m.Label({
-							text : "Password don't match..."
-						}), new sap.m.Button({
-							type : sap.m.ButtonType.Emphasized,
-							text : "OK",
-							press : function() {
-								dialog.close();
-							}
-						}).addStyleClass("btnError") ]
-					});
-					dialog.open();
-				}
-
-			} else {
-				var dialog = new sap.m.Dialog({
-
-					title : "Error",
-					state : sap.ui.core.ValueState.Error,
-					content : [ new sap.m.Label({
-						text : "you need to complete the data."
-					}),
-
-					new sap.m.Button({
-						type : sap.m.ButtonType.Emphasized,
-						text : "OK",
-						press : function() {
-							dialog.close();
-						}
-					}).addStyleClass("btnError2") ]
-				});
-				dialog.open();
-
-			}
-		}
-
-	},
-
-/**
- * Called when the Controller is destroyed. Use this one to free resources and
- * finalize activities.
- * 
- * @memberOf timebee.accountcreate
- */
-// onExit: function() {
-//
-// }
+	}
 });
